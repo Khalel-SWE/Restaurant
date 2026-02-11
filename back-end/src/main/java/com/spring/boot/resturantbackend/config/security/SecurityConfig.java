bce60b26e59d18c6d93c217e420803bb496c8579
Khalel-SWE <khalel.swe96@gmail.com>
2026-02-11 18:58:05 +0100

    Update SecurityConfig.java

--- back-end/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
+++ back-end/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -33,7 +32,7 @@ public class SecurityConfig {
                         .requestMatchers("/v3/**").permitAll()
                         .anyRequest().authenticated()
         );
-        http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+//        http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
