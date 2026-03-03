137f2b5ab0921681fa44f2d373515aaead5794cb
Khalel-SWE <khalel.swe96@gmail.com>
2026-03-03 21:51:40 +0100

    commit
    delete some lines

--- back-end/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
+++ back-end/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**").permitAll()
 //                        .requestMatchers("/swagger-ui/**").permitAll()
 //                        .requestMatchers("/v3/**").permitAll()
-                        // we canceled this to make things work without rules for now at least
                         .anyRequest().authenticated()
         );
 
