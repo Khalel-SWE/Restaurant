dd5d4668f5a5bb7ba9302fea4a674a751beec3c2
Khalel-SWE <khalel.swe96@gmail.com>
2026-03-05 21:43:16 +0100

    back-end and front-end
    connection

--- back-end/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
+++ back-end/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
@@ -28,7 +28,6 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(
                 authorizeRequests -> authorizeRequests
-                        // this line to make the account auth
                         .requestMatchers("/auth/**").permitAll()
 //                        .requestMatchers("/swagger-ui/**").permitAll()
 //                        .requestMatchers("/v3/**").permitAll()
