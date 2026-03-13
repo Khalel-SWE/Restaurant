aa19a419e876af7ac776acc7d885eca8d692370e
Khalel-SWE <khalel.swe96@gmail.com>
2026-03-13 12:40:37 +0100

    Update SecurityConfig.java

--- back-end/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
+++ back-end/src/main/java/com/spring/boot/resturantbackend/config/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
         http.securityMatcher("/**").cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
         http.authorizeHttpRequests(
+                // user and admin auth
                 authorizeRequests -> authorizeRequests
                         .requestMatchers("/auth/**").permitAll()
 //                        .requestMatchers("/swagger-ui/**").permitAll()
