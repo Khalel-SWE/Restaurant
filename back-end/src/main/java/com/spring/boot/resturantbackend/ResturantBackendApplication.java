18290e3b35ec9c6e795ad1e650980d51d093aecf
Khalel-SWE <khalel.swe96@gmail.com>
2026-04-10 23:51:59 +0200

    Update ResturantBackendApplication.java

--- back-end/src/main/java/com/spring/boot/resturantbackend/ResturantBackendApplication.java
+++ back-end/src/main/java/com/spring/boot/resturantbackend/ResturantBackendApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class ResturantBackendApplication {
 
     public static void main(String[] args) {
+        // admin
+        //
         SpringApplication.run(ResturantBackendApplication.class, args);
     }
 
