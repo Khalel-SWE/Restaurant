933a71c1978acb2d21f971510a92c896fa55a8cf
Khalel-SWE <khalel.swe96@gmail.com>
2026-04-18 21:59:21 +0200

    Add DTO comment to AccountMapper
    Insert a brief '// DTO' comment above the ACCOUNT_MAPPER field in AccountMapper to clarify its purpose. This is a non-functional, documentation-only change to improve code readability.

--- back-end/src/main/java/com/spring/boot/resturantbackend/mappers/security/AccountMapper.java
+++ back-end/src/main/java/com/spring/boot/resturantbackend/mappers/security/AccountMapper.java
@@ -9,6 +9,7 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface AccountMapper {
+    // DTO
     AccountMapper ACCOUNT_MAPPER = Mappers.getMapper(AccountMapper.class);
 
     AccountDto toAccountDto(Account account);
