2a4a037e55b449e83c074c3053005507657a5d7d
Khalel-SWE <khalel.swe96@gmail.com>
2026-04-25 09:06:25 +0200

    Comment out ResponseOrderVm three-arg constructor
    Commented out the constructor in ResponseOrderVm that accepted (String code, Double totalPrice, Double totalNumber). The code is retained as commented lines for potential future reference; no functional behavior was changed.

--- back-end/src/main/java/com/spring/boot/resturantbackend/controllers/vm/ResponseOrderVm.java
+++ back-end/src/main/java/com/spring/boot/resturantbackend/controllers/vm/ResponseOrderVm.java
@@ -28,4 +28,9 @@ public class ResponseOrderVm {
         this.message = message;
     }
 
+//    public ResponseOrderVm(String code, Double totalPrice, Double totalNumber) {
+//        this.code = code;
+//        this.totalPrice = totalPrice;
+//        this.totalNumber = totalNumber;
+//    }
 }
